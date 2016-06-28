c5d70d3538c1876838b2e9feea9afe514af75e7a
jiahao <monkey19911021@gmail.com>
2016-06-28 14:31:24 +0800

    出屎化

---
+++ MKDocumentProvider/MKDocumentProvider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKDocumentProvider
+//
+//  Created by DONLINKS on 16/6/28.
+//  Copyright © 2016年 Donlinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MKDocumentProvider/MKDocumentProvider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MKDocumentProvider
+//
+//  Created by DONLINKS on 16/6/28.
+//  Copyright © 2016年 Donlinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
